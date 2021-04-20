1bf88e04eb485151adb5069dbb5ead19a25b2d97
Shivakoreddi <>
2021-04-19 22:34:31 -0400

    commit

--- FCA.py
+++ FCA.py
@@ -1,15 +1,37 @@
-from flask import Flask
+from flask import Flask,render_template,url_for
 import json
 app = Flask(__name__)
 
+posts = [
+    {
+        "title":"Forum post 1",
+        "author": "shiva Koreddi",
+        "date_posted":"04/19/2021",
+        "content":"First post content"
+
+    },
+    {
+        "title":"Forum Post 2",
+        "author": "Sravani",
+        "date_posted": "04/19/2021",
+        "content":"Second post content"
+
+    }
+]
 class Forum():
+    @app.route("/home")
+    def home():
+        return render_template('home.html',posts= posts)
+
+    @app.route("/about")
+    def about():
+        return render_template('about.html')
     @app.route("/snippets")
     def get():
         return {"name":"recipe", "expires_in": 30, "snippet":"1 apple"}
 
     @app.route("/recipe")
-    def post(data):
-        return data
-
+    def post(self):
+        return self.content
 if  __name__=="__main___":
     app.run(debug=True)
--- snip.py
+++ snip.py
@@ -1,8 +1,7 @@
-import FCA
 import requests
 url = 'http://127.0.0.1:5000/recipe'
 ##response = Forum.post(url,data={"name":"recipe", "expires_in": 30, "snippet":"1 apple"})
 ##response = requests.get(url)
-
-response = requests.post(url,data={"name":"recipe", "expires_in": 30, "snippet":"1 apple"})
+myobj= {"name":"recipe", "expires_in": 30, "snippet":"1 apple"}
+response = requests.post(url,data=myobj)
 print(response.text)
